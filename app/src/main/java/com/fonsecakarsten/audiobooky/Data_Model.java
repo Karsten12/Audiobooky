d3ef18b8f85cb9fd895ede29ba83ff8c8e1ed671
Karsten <karstenfonseca@gmail.com>
2017-06-05 19:35:34 -0700

    Test camera

---
+++ app/src/main/java/com/fonsecakarsten/audiobooky/Data_Model.java
@@ -0,0 +1,22 @@
+package com.fonsecakarsten.audiobooky;
+
+/**
+ * Created by Karsten on 6/5/2017.
+ */
+
+public class Data_Model {
+    private String image;
+
+
+    // Getter and Setter model for recycler view items
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+
+}
