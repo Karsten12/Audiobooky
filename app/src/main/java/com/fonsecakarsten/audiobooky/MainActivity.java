93a291958064f36e47b3a3ec72c5ee35649349b3
kfonseca <karstenfonseca@gmail.com>
2017-08-23 22:57:04 -0700

    - edited some Firebase details

--- app/src/main/java/com/fonsecakarsten/audiobooky/MainActivity.java
+++ app/src/main/java/com/fonsecakarsten/audiobooky/MainActivity.java
@@ -30,6 +30,7 @@ import com.fonsecakarsten.audiobooky.Database.BookDbHelper;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -39,6 +40,8 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 
@@ -103,7 +106,8 @@ public class MainActivity extends AppCompatActivity {
         captureBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                addNewBook();
+//                addNewBook();
+                signIn();
             }
         });
     }
@@ -194,6 +198,13 @@ public class MainActivity extends AppCompatActivity {
                 BookInfoAsync task = new BookInfoAsync(data.getStringExtra("BarCodeString"), getApplicationContext(), MainActivity.this);
                 task.execute();
             }
+        } else if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
@@ -226,13 +237,10 @@ public class MainActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
-                            //Log.d(TAG, "signInWithCredential:success");
                             FirebaseUser user = mAuth.getCurrentUser();
                         } else {
                             // If sign in fails, display a message to the user.
-                            //Log.w(TAG, "signInWithCredential:failure", task.getException());
-                            Toast.makeText(MainActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
+                            Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
                             //updateUI(null);
                         }
 
@@ -241,6 +249,12 @@ public class MainActivity extends AppCompatActivity {
                         // [END_EXCLUDE]
                     }
                 });
+    }
+
+
+    private void uploadData() {
+        FirebaseUser mFirebaseUser = mAuth.getCurrentUser();
+        StorageReference storageRef = FirebaseStorage.getInstance().getReference();
     }
 
 
